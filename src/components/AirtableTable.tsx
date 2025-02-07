f6d733948145a6a3b3289c5c94a8af3e397a0f2e
visionaryaa <digiteo.info@gmail.com>
2025-02-07 16:46:50 +0100

    Update AirtableTable.tsx
    Added robertHalf logo

--- src/components/AirtableTable.tsx
+++ src/components/AirtableTable.tsx
@@ -57,6 +57,10 @@ const AGENCY_LOGOS = [
   {
     domain: 'sdworx.jobs',
     logo: 'https://i.postimg.cc/XJ8FtyxC/339105639-183429217812911-8132452130259136190-n.png'
+  },
+  {
+    domain: 'roberthalf.com',
+    logo: 'https://i.postimg.cc/13vSMqjT/383209240-608879378108206-6829050048883403071-n.jpg'
   }
 
   
